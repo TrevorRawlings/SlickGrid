e8eda513c2c68391f2734058f335823ee5b4eba3
Trevor Rawlings <t@eggrack.co.uk>
2012-12-06 12:25:52 +0000

    Corrected formatting

--- slick.grid.js
+++ slick.grid.js
@@ -1412,9 +1412,9 @@ if (typeof Slick === "undefined") {
         cellCss += (" active");
       }
 
-        if (metadata && metadata.cssCellClasses && metadata.cssCellClasses[m.id]) {
-            cellCss += " " + metadata.cssCellClasses[m.id]
-        }
+      if (metadata && metadata.cssCellClasses && metadata.cssCellClasses[m.id]) {
+        cellCss += " " + metadata.cssCellClasses[m.id];
+      }
 
       // TODO:  merge them together in the setter
       for (var key in cellCssClasses) {
